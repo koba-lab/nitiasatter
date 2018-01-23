4629a5e92f418c16baf58a1d3768614e5db9d902
wtrkbys <kobayashi.wataru@donika.co.jp>
2018-01-23 14:26:46 +0900

    ヘッダーをクソシンプルにします

--- views/layouts/_header.php
+++ views/layouts/_header.php
@@ -5,34 +5,19 @@ use yii\helpers\Html;
 /**
  * @var $this yii\web\View
  */
+$this->registerCss('
+.l-header {
+    height: 50px;
+}
+');
 ?>
-<?php
-NavBar::begin([
-    'brandLabel' => Yii::$app->name,
-    'brandUrl' => Yii::$app->homeUrl,
-    'options' => [
-        'class' => 'navbar-inverse navbar-fixed-top',
-    ],
-]);
-echo Nav::widget([
-    'options' => ['class' => 'navbar-nav navbar-right'],
-    'items' => [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-        Yii::$app->user->isGuest ? (
-            ['label' => 'Login', 'url' => ['/site/login']]
-        ) : (
-            '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>'
-        )
-    ],
-]);
-NavBar::end();
-?>
+<header>
+    <div class="l-header container d-flex align-items-center">
+        <div class="mr-auto">
+            <?= Html::a(Yii::$app->name, '/', ['class' => 'd-block brand']) ?>
+        </div>
+        <div class="ml-auto">
+            <!-- 右カラム -->
+        </div>
+    </div>
+</header>
