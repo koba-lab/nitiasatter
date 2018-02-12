68901995e57e2d2c02894de1f449c1aeed77db4a
小林 航 <wataruags@gmail.com>
2018-02-12 15:29:31 +0900

    faviconを置きました

--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -36,7 +36,7 @@ apt-get install -y software-properties-common python-software-properties
 apt-get upgrade -y
 
 info "Install additional software"
-apt-get install -y php7.1-curl php7.1-cli php7.1-intl php7.1-mysqlnd php7.1-gd php7.1-fpm php7.1-mbstring php7.1-xml unzip nginx mysql-server-5.7 nodejs build-essential yarn
+apt-get install -y php7.1-curl php7.1-cli php7.1-intl php7.1-mysqlnd php7.1-gd php7.1-fpm php7.1-mbstring php7.1-xml unzip nginx mysql-server-5.7 nodejs build-essential yarn git
 
 info "Configure MySQL"
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
